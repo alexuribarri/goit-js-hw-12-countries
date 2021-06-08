fdfd64e46e8e90f6f6fc116ad2e2dbe2ece8fdbe
Alex Uribarri <alex@uribarri.es>
2021-06-09 00:30:20 +0200

    updated everything except alerts

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,18 +1,36 @@
-import {printCountries} from './io';
-import {alert, defaultModules}  from '../../node_modules/@pnotify/core/dist/PNotify';
-const getCountries = (name) => {fetch(`https://restcountries.eu/rest/v2/name/${name}`).then(r=>{return r.json()}).then(cObject=> {displaySelector(cObject)})}
+import {printCountries, clearDisplay, printCountryCard} from './io';
+import {error, notice}  from '../../node_modules/@pnotify/core/dist/PNotify';
+import '@pnotify/core/dist/BrightTheme.css';
+const getCountries = (name) => {fetch(`https://restcountries.eu/rest/v2/name/${name}`).then(r=>{return r.json()}).catch(r=>{console.log(r)}).then(cObject=> {displaySelector(cObject)})}
+
+//const getCountries = (name) => {fetch(`https://restcountries.eu/rest/v2/name/${name}`).then(r=>{return r.json()}).then(cObject=> {displaySelector(cObject)})}
+
+
 const displaySelector=(array) => {
     if (array.length===1) {
-        console.log('show card')
+        clearDisplay()
+        printCountryCard(array)
     }
     else if (array.length<=10) {
+        clearDisplay()
         printCountries(array)
     }
     else if (array.length>10) {
-        alert({
-            text: 'Notice me, senpai!'
-          });
-    }
+        clearDisplay()
 
+        notice({
+            title: 'Attention',
+            text: 'Toom many results, please narrow your search!'
+          })}
+
+    else if (array.length<1) {
+            clearDisplay()
+    
+            error({
+                title: 'Attention',
+                text: 'eewfew!'
+              })};
+  
+    
 }
 export default getCountries;
--- src/js/io.js
+++ src/js/io.js
@@ -2,6 +2,7 @@ import Debounce from 'lodash';
 import cArray from './fetchCountries'
 const inputEl = document.querySelector('input')
 const listEl = document.querySelector('ul')
+const countryCard=document.querySelector('.country')
 let name;
 
 
@@ -11,6 +12,16 @@ const onInput=()=>{
    cArray(name);
    }
 
+//clear the field
+
+const clearDisplay = () => {
+    listEl.innerHTML='';
+    countryCard.innerHTML='';
+}
+
+
+
+//print list of countries
 const printCountries = (countryList) => {
     const listHTML = [];
     countryList.map(country=> {
@@ -21,9 +32,42 @@ const printCountries = (countryList) => {
 
 } 
 
+
+//print country card
+
+const printCountryCard = (countryName) => {
+    console.log(countryName)
+
+    const langList = [];
+    countryName[0].languages.map(language=> {
+        langList.push(`<li>${language.name}</li>`)
+    })
+    countryCard.innerHTML=`
+    <h1>${countryName[0].name}</h1>    
+            <div class='country_card'>
+                <div class="left">
+                    <ul>
+                        <li class="details"><p style="font-weight: 700;">Capital:&nbsp</p><p>${countryName[0].capital}</p></li>
+                        <li class="details"><p style="font-weight: 700;">Population:&nbsp</p><p>${countryName[0].population}</p></li>
+                        <li class='details_last'><p style="font-weight: 700;">Languages:</p>
+                        <ul class="languages">
+                       ${langList.join('')}
+                        </ul>
+                    
+                    </li>
+                    </ul>
+                    
+                    </div>
+                    <div class="right">
+                    <img src="${countryName[0].flag}" alt="${countryName[0].name}" width="300px">
+                </div>
+            </div>
+    `
+}
+
 inputEl.addEventListener('input', _.debounce(onInput, 500))
 
 
 
 
-export {printCountries};
+export {printCountries, clearDisplay, printCountryCard};
