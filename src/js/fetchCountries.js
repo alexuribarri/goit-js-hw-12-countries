444109e97b08c693fe0de9e28f022c144d0a6b4b
Alex Uribarri <alex@uribarri.es>
2021-06-11 22:35:29 +0200

    final

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,8 @@
 import {printCountries, clearDisplay, printCountryCard} from './io';
 import {error, notice}  from '../../node_modules/@pnotify/core/dist/PNotify';
-import '@pnotify/core/dist/BrightTheme.css';
+import '../../node_modules/@pnotify/core/dist/PNotify.css';
+import '../../node_modules/@pnotify/core/dist/BrightTheme.css';
+
 const getCountries = (name) => {fetch(`https://restcountries.eu/rest/v2/name/${name}`).then(r=>{return r.json()}).catch(r=>{console.log(r)}).then(cObject=> {displaySelector(cObject)})}
 
 //const getCountries = (name) => {fetch(`https://restcountries.eu/rest/v2/name/${name}`).then(r=>{return r.json()}).then(cObject=> {displaySelector(cObject)})}
@@ -18,7 +20,7 @@ const displaySelector=(array) => {
     else if (array.length>10) {
         clearDisplay()
 
-        notice({
+        error({
             title: 'Attention',
             text: 'Toom many results, please narrow your search!'
           })}
